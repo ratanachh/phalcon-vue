812ac863b8bafc0aa1bff8db3f5506b90e12db6d
Ratana <chhormratana@gmail.com>
2020-06-24 13:39:47 +0700

    add composer.json

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use Phalcon\Di\FactoryDefault;
-use Phalcon\Mvc\Application;
 
 error_reporting(E_ALL);
 
@@ -10,40 +8,18 @@ define('BASE_PATH', dirname(__DIR__));
 define('APP_PATH', BASE_PATH . '/src');
 
 try {
-    /**
-     * The FactoryDefault Dependency Injector automatically registers
-     * the services that provide a full stack framework.
-     */
-    $di = new FactoryDefault();
-
-    /**
-     * Read services
-     */
-    require_once BASE_PATH . '/config/services.php';
-
-    /**
-     * Handle routes
-     */
-    require_once BASE_PATH . '/config/router.php';
-
-
-    /**
-     * Get config service for use in inline setup below
-     */
-    $config = $di->getConfig();
-
-    /**
-     * Include Autoloader
-     */
     require_once BASE_PATH . '/config/loader.php';
 
     /**
-     * Handle the request
+     * Load .env configurations
      */
-    unset($_GET['_url']);
-    $application = new Application($di);
-    $application->handle(BASE_URI)->send();
-} catch (\Exception $e) {
-    echo $e->getMessage() . '<br>';
-    echo '<pre>' . $e->getTraceAsString() . '</pre>';
+    Dotenv\Dotenv::create(BASE_PATH)->load();
+
+    /**
+     * Run Application!
+     */
+    echo (new \App\Application(BASE_PATH))->run();
+} catch (Exception $e) {
+    echo $e->getMessage(), '<br>';
+    echo nl2br(htmlentities($e->getTraceAsString()));
 }
